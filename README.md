8a63466019a3cdd66674100a1cada0934f500105
CabbSir <cabbsir@gmail.com>
2022-06-14 20:57:02 +0800

    提交成品

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # scripts
 
-一些杂乱的小脚本 | 文件 | 内容 | | --- | --- | |metashape.py | metashape自动重建脚本 | |3df.py | 3df自动重建脚本 | |3df-xiao.py |
-仅适用于肖哥电脑的3df重建脚本 | |down-sampling.py | 下采样点云文件 | |format.py | 点云格式转换工具|
+| 文件                | 内容            |
+|-------------------|---------------| 
+| metashape.py      | metashape自动重建脚本 | 
+| 3df.py            | 3df自动重建脚本     | 
+| 3df-xiao.py       | 仅适用于肖哥电脑的3df重建脚本 | 
+| down-sampling.py  | 下采样点云文件       | 
+| format.py         | 点云格式转换工具      |
--- format.py
+++ format.py
@@ -1,3 +1,8 @@
+"""
+将 ".ply", ".pcd", ".txt" 三种点云格式转换成其他任意四种
+"""
+
+import os
 from tkinter import *
 from tkinter.filedialog import askdirectory, askopenfilenames
 import open3d as o3d
@@ -5,16 +10,24 @@ import numpy as np
 
 # 常量设置
 input_type = ["点云", "网格"]
-category = [".ply", ".pcd", ".obj", ".txt"]
+category = [".ply", ".pcd", ".xyz"]
+is_dir = False
+files = ()
 
 
 def select_input_path():
+    global is_dir
+    is_dir = True
     input_path_ = askdirectory()
     input_path.set(input_path_)
 
 
 def select_input_files():
+    global is_dir
+    is_dir = False
     input_files_ = askopenfilenames()
+    global files
+    files = input_files_
     input_path.set(input_files_)
 
 
@@ -23,31 +36,53 @@ def select_output_path():
     output_path.set(output_path_)
 
 
-def select_output_files():
-    output_files_ = askopenfilenames()
-    output_path.set(output_files_)
+def mesh_to_pc():
+    if is_dir:
+        temp = os.listdir(input_path.get())
+    else:
+        temp = files
+    for file in temp:
+        # 只有这四种文件处理
+        if ".ply" in file or ".pcd" in file or ".obj" in file or ".txt" in file:
+            if is_dir:
+                mesh = o3d.io.read_triangle_mesh(os.path.join(input_path.get(), file))
+            else:
+                mesh = o3d.io.read_triangle_mesh(file)
+            points = np.asarray(mesh.vertices)
+            pcd = o3d.geometry.PointCloud()
+            pcd.points = o3d.utility.Vector3dVector(points)
+            if is_dir:
+                output = os.path.join(output_path.get(), file.split(".")[0] + category[output_var.get()])
+            else:
+                output = os.path.join(output_path.get(),
+                                      os.path.split(file)[1].split(".")[0] + category[output_var.get()])
+            o3d.io.write_point_cloud(output, pcd)
 
 
-def set_input():
-    print(input_type[input_var.get()])
-
-
-def set_output():
-    print(category[output_var.get()])
+def pc_to_pc():
+    if is_dir:
+        temp = os.listdir(input_path.get())
+    else:
+        temp = files
+    for file in temp:
+        # 只有这四种文件处理
+        if ".ply" in file or ".pcd" in file or ".obj" in file or ".txt" in file:
+            if is_dir:
+                pcd = o3d.io.read_point_cloud(os.path.join(input_path.get(), file))
+                output = os.path.join(output_path.get(), file.split(".")[0] + category[output_var.get()])
+            else:
+                pcd = o3d.io.read_point_cloud(file)
+                output = os.path.join(output_path.get(),
+                                      os.path.split(file)[1].split(".")[0] + category[output_var.get()])
+            o3d.io.write_point_cloud(output, pcd, print_progress=True)
 
 
 def format():
-    print(input_var.get())
-    print(output_var.get())
-    mesh = o3d.io.read_triangle_mesh("/Users/cabbsir/Downloads/pcd/m82D_highlight_C_D08.obj")
-    points = np.asarray(mesh.vertices)
-    pcd = o3d.geometry.PointCloud()
-    # pcd.colors = o3d.utility.Vector3dVector([1, 0, 0])
-    pcd.points = o3d.utility.Vector3dVector(points)
-    # pcd.paint_uniform_color([1, 0, 0])
-    print(pcd)
-    o3d.io.write_point_cloud("/Users/cabbsir/Downloads/pcd/D08.ply", pcd)
-    o3d.visualization.draw_geometries([pcd])
+    # 验证是否选择的没有问题
+    if input_var.get() == 0:
+        pc_to_pc()
+    else:
+        mesh_to_pc()
 
 
 root = Tk()
@@ -60,19 +95,18 @@ Label(root, text="目标路径:").grid(row=0, column=0)
 Entry(root, textvariable=input_path).grid(row=0, column=1)
 Button(root, text="路径选择", command=select_input_path).grid(row=0, column=2)
 Button(root, text="文件选择", command=select_input_files).grid(row=0, column=3)
-input_label = Label(root, text="输入点云格式:").grid(row=1, column=0)
+Label(root, text="输入点云格式:").grid(row=1, column=0)
 
 for i in range(len(input_type)):
-    Radiobutton(root, text=input_type[i], variable=input_var, value=i, command=set_input).grid(row=1, column=i + 1)
+    Radiobutton(root, text=input_type[i], variable=input_var, value=i).grid(row=1, column=i + 1)
 
 Label(root, text="输出路径:").grid(row=3, column=0)
 Entry(root, textvariable=output_path).grid(row=3, column=1)
 Button(root, text="路径选择", command=select_output_path).grid(row=3, column=2)
-Button(root, text="文件选择", command=select_output_files).grid(row=3, column=3)
-output_label = Label(root, text="输出点云格式:").grid(row=4, column=0)
+Label(root, text="输出点云格式:").grid(row=4, column=0)
 
 for i in range(len(category)):
-    Radiobutton(root, text=category[i], variable=output_var, value=i, command=set_input).grid(row=4, column=i + 1)
+    Radiobutton(root, text=category[i], variable=output_var, value=i).grid(row=4, column=i + 1)
 
 Button(root, text="开始转换", command=format).grid(row=5, column=4)
 
